bea00c53214dff2365ccd5f15aa86bc70c78d94b
jakebark <jakebark@amazon.co.uk>
2025-03-13 17:05:37 +0000

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "bucket" {
   value = aws_s3_bucket.this
 }
+
+output "cwl_group" {
+  value = aws_cloudwatch_log_group.this
+}
+
+output "codebuild_project" {
+  value = aws_codebuild_project.this
+}
